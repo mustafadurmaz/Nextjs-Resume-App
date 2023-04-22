5c38f1f5fa892acad839bce3bc98560e7b613543
mustafadurmaz <mdmz_001@hotmail.com>
2023-04-22 23:02:02 +0300

    Sidebar edit

--- app/component/sidebar.jsx
+++ app/component/sidebar.jsx
@@ -1,28 +1,33 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
-import {Envelope, Twitter, Linkedin} from "./icons"
+import { Envelope, Twitter, Linkedin } from "./icons";
 
-const Sidebar = ({data}) => {
-
-  const {name,role,education, contactLinks}=data;
+const Sidebar = ({ data }) => {
+  const { name, role, education, contactLinks } = data;
   return (
-    <div className='bg-red'>
-      <div>
-        {/* <Image/> */}
+    <div className="bg-black flex flex-col content-between w-full h-auto sm:h-screen sm:justify-around sm:w-1/3 sm:fixed">
+      <div className="text-white flex flex-col p-10">
+        <Image />
         <h1>{name}</h1>
         <h2>{role}</h2>
         <p>{education[0]}</p>
         <p>{education[1]}</p>
         <div>
           <h3>Contact me</h3>
-          <a href={contactLinks?.[0]} aria-label={"twitter"}><Envelope /></a>
-          <a href={contactLinks?.[0]} aria-label={"twitter"}><Twitter /></a>
-          <a href={contactLinks?.[0]} aria-label={"twitter"}><Linkedin /></a>
-
+          <a href={contactLinks?.[0]} aria-label={"twitter"}>
+            <Envelope />
+          </a>
+          <a href={contactLinks?.[0]} aria-label={"twitter"}>
+            <Twitter />
+          </a>
+          <a href={contactLinks?.[0]} aria-label={"twitter"}>
+            <Linkedin />
+          </a>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
