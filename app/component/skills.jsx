1b7db9e2659f9d6f806c321020f35dc595ed4d00
mustafadurmaz <mdmz_001@hotmail.com>
2023-05-02 22:21:03 +0300

    Skills page edit

--- app/component/skills.jsx
+++ app/component/skills.jsx
@@ -2,18 +2,21 @@
 import React, { useState } from "react";
 
 const Skills = () => {
-  const [activeTab, setActiveTab] = useState("Soft");
+  const [activeTab, setActiveTab] = useState("soft");
   const setBg=(active)=>(
     activeTab===active ? "bg-yellow" : "bg-grey"
   )
-  console.log(activeTab);
+  
+  const setTabsAlignment=(tab)=>(
+    tab==="soft" ? "text-left" : "text-right"
+  )
   return (
     <div className="flex">
-      {["Soft", "Hard"].map((item) => (
+      {["soft", "hard"].map((item) => (
         <button
           key={item}
           type="button"
-          className={`btn ${setBg(item)}`}
+          className={`btn ${setBg(item)} ${setTabsAlignment(item)}`}
           onClick={() => setActiveTab(item)}
         >
           {item} Skilss
